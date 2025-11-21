9b16cd581d1bdad111128ea60d5a78c337da2920
Carsten Clauss <clauss@par-tec.com>
2025-11-21 12:41:53 +0100

    test/parastation/datatype/ucc: Add new tests for derived dtypes.
    This commit adds a whole set of new tests for the use of "mixed
    datatypes". The term "mixed datatypes" here refers to those cases
    where, for example, there are contiguous datatypes on the sender
    side (e.g. such as simple buffer filled with basic datatypes) but
    have gaps on the receiver side -- or vice versa.
    
    The motivation for these tests comes primarily from UCC support
    and the problem there of coherently mapping such cases to basic
    datatypes in order to be able to make still use of UCC. Therefore,
    all these tests make currently only use of collective operations
    and not pt2pt communication.
    
    The tests are divided into four different classes for this purpose:
    `mixed_basic_to_derived`, `mixed_contig_to_derived`,
    `mixed_derived_to_basic`, `mixed_derived_to_contig`.
    These in turn provide support for most relevant collectives, which
    can be selected by defining a corresponding macro:
    `BCAST`, `GATHER`, `GATHERV`, `ALLGATHER`, `ALLGATHERV`,
    `SCATTER`, `SCATTERV`, `ALLTOALL`, `ALLTOALLV`.
    The Makefile ensures that all class/collop combinations are built.
    
    Additionally, in order to explicitly test the UCC case, the new
    test set `ps-test-ucc` is introduced, which also contains some
    other tests in other folders with `PSP_UCC=1` enabled.

---
+++ mpich2/test/mpi/parastation/datatype/mixed_derived_to_basic.c
@@ -0,0 +1,287 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2026 ParTec AG, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ */
+
+#include "mixed_datatype_test.h"
+
+int main(int argc, char **argv)
+{
+    int errs = 0;
+
+    MTest_Init(&argc, &argv);
+
+    int rank, nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    /* Init global vars */
+    init_datatype(0, nprocs);
+
+#if defined(ALLGATHER)
+    sbuffer = malloc(factor * count * stride * basic_size);
+    rbuffer = malloc(factor * count * blocklength * basic_size * nprocs);
+
+    for (int i = 0; i < factor * count * stride; i++) {
+        sbuffer[i] = (type_basic_t) i + rank;
+    }
+
+    test_handle(MPI_Allgather
+                (sbuffer, factor, type_derived, rbuffer, factor * count * blocklength,
+                 type_basic, MPI_COMM_WORLD));
+
+    for (int i = 0; i < nprocs; i++) {
+        for (int j = 0; j < factor * count * blocklength; j++) {
+            int pos = j + factor * count * blocklength * i;
+            test_assert(rbuffer[pos] == 1.0 * j + (j / blocklength) * (stride - blocklength) + i,
+                        "mismatch at position %d", pos);
+        }
+    }
+#elif defined(ALLGATHERV)
+    sbuffer = malloc(factor * count * stride * basic_size);
+    rbuffer = malloc((factor * count * blocklength + 1) * basic_size * nprocs);
+
+    for (int i = 0; i < nprocs; i++) {
+        rcounts[i] = factor * count * blocklength;
+        rdispls[i] = (i * factor * count * blocklength) + i;
+    }
+
+    for (int i = 0; i < factor * count * stride; i++) {
+        sbuffer[i] = (type_basic_t) i + rank;
+    }
+
+    test_handle(MPI_Allgatherv
+                (sbuffer, factor, type_derived, rbuffer, rcounts, rdispls, type_basic,
+                 MPI_COMM_WORLD));
+
+    for (int i = 0; i < nprocs; i++) {
+        for (int j = 0; j < factor * count * blocklength; j++) {
+            int pos = (j + factor * count * blocklength * i) + i;
+            test_assert(rbuffer[pos] == 1.0 * j + (j / blocklength) * (stride - blocklength) + i,
+                        "mismatch at position %d", pos);
+        }
+    }
+#elif defined(ALLTOALL)
+    sbuffer = malloc(factor * count * stride * basic_size * nprocs);
+    rbuffer = malloc(factor * count * blocklength * basic_size * nprocs);
+
+    for (int i = 0; i < nprocs; i++) {
+        for (int j = 0; j < factor * count * stride; j++) {
+            int pos = i * (factor * count * stride) + j;
+            sbuffer[pos] = (type_basic_t) j + rank;
+        }
+    }
+
+    test_handle(MPI_Alltoall
+                (sbuffer, factor, type_derived, rbuffer, factor * count * blocklength,
+                 type_basic, MPI_COMM_WORLD));
+
+    for (int i = 0; i < nprocs; i++) {
+        for (int j = 0; j < factor * count * blocklength; j++) {
+            int pos = j + factor * count * blocklength * i;
+            test_assert(rbuffer[pos] == 1.0 * j + (j / blocklength) * (stride - blocklength) + i,
+                        "mismatch at position %d", pos);
+        }
+    }
+#elif defined(ALLTOALLV)
+    sbuffer = malloc(factor * count * stride * basic_size * nprocs);
+    rbuffer = malloc((factor * count * blocklength + 1) * basic_size * nprocs);
+
+    for (int i = 0; i < nprocs; i++) {
+        scounts[i] = factor;
+        sdispls[i] = i * factor;
+        rcounts[i] = factor * count * blocklength;
+        rdispls[i] = (i * factor * count * blocklength) + i;
+    }
+
+    for (int i = 0; i < nprocs; i++) {
+        for (int j = 0; j < factor * count * stride; j++) {
+            int pos = i * (factor * count * stride) + j;
+            sbuffer[pos] = (type_basic_t) j + rank;
+        }
+    }
+
+    test_handle(MPI_Alltoallv
+                (sbuffer, scounts, sdispls, type_derived, rbuffer, rcounts, rdispls,
+                 type_basic, MPI_COMM_WORLD));
+
+    for (int i = 0; i < nprocs; i++) {
+        for (int j = 0; j < factor * count * blocklength; j++) {
+            int pos = (j + factor * count * blocklength * i) + i;
+            test_assert(rbuffer[pos] == 1.0 * j + (j / blocklength) * (stride - blocklength) + i,
+                        "mismatch at position %d", pos);
+        }
+    }
+#elif defined(BCAST)
+    if (rank == root) {
+
+        sbuffer = malloc(factor * count * stride * basic_size);
+
+        for (int i = 0; i < factor * count * stride; i++) {
+            sbuffer[i] = (type_basic_t) i;
+        }
+
+        MPI_Bcast(sbuffer, factor, type_derived, root, MPI_COMM_WORLD);
+
+    } else {
+
+        rbuffer = malloc(factor * count * blocklength * basic_size);
+
+        test_handle(MPI_Bcast
+                    (rbuffer, factor * count * blocklength, type_basic, root, MPI_COMM_WORLD));
+
+        for (int i = 0; i < factor * count * blocklength; i++) {
+            test_assert(rbuffer[i] == 1.0 * i + (i / blocklength) * (stride - blocklength),
+                        "mismatch at position %d", i);
+        }
+    }
+#elif defined(GATHER)
+    if (rank == root) {
+
+        sbuffer = malloc(factor * count * stride * basic_size);
+        rbuffer = malloc(factor * count * blocklength * basic_size * nprocs);
+
+        for (int i = 0; i < factor * count * stride; i++) {
+            sbuffer[i] = (type_basic_t) i + rank;
+        }
+
+        test_handle(MPI_Gather
+                    (sbuffer, factor, type_derived, rbuffer, factor * count * blocklength,
+                     type_basic, root, MPI_COMM_WORLD));
+
+        for (int i = 0; i < nprocs; i++) {
+            for (int j = 0; j < factor * count * blocklength; j++) {
+                int pos = j + factor * count * blocklength * i;
+                test_assert(rbuffer[pos] ==
+                            1.0 * j + (j / blocklength) * (stride - blocklength) + i,
+                            "mismatch at position %d", pos);
+            }
+        }
+
+    } else {
+
+        sbuffer = malloc(factor * count * stride * basic_size);
+
+        for (int i = 0; i < factor * count * stride; i++) {
+            sbuffer[i] = (type_basic_t) i + rank;
+        }
+
+        MPI_Gather(sbuffer, factor, type_derived, NULL, 0, MPI_DATATYPE_NULL, root, MPI_COMM_WORLD);
+    }
+#elif defined(GATHERV)
+    if (rank == root) {
+
+        sbuffer = malloc(factor * count * stride * basic_size);
+        rbuffer = malloc((factor * count * blocklength + 1) * basic_size * nprocs);
+
+        for (int i = 0; i < nprocs; i++) {
+            rcounts[i] = factor * count * blocklength;
+            rdispls[i] = (i * factor * count * blocklength) + i;
+        }
+
+        for (int i = 0; i < factor * count * stride; i++) {
+            sbuffer[i] = (type_basic_t) i + rank;
+        }
+
+        test_handle(MPI_Gatherv
+                    (sbuffer, factor, type_derived, rbuffer, rcounts, rdispls, type_basic,
+                     root, MPI_COMM_WORLD));
+
+        for (int i = 0; i < nprocs; i++) {
+            for (int j = 0; j < factor * count * blocklength; j++) {
+                int pos = (j + factor * count * blocklength * i) + i;
+                test_assert(rbuffer[pos] ==
+                            1.0 * j + (j / blocklength) * (stride - blocklength) + i,
+                            "mismatch at position %d", pos);
+            }
+        }
+
+    } else {
+
+        sbuffer = malloc(factor * count * stride * basic_size);
+
+        for (int i = 0; i < factor * count * stride; i++) {
+            sbuffer[i] = (type_basic_t) i + rank;
+        }
+
+        MPI_Gatherv(sbuffer, factor, type_derived, NULL, NULL, NULL, MPI_DATATYPE_NULL, root,
+                    MPI_COMM_WORLD);
+    }
+#elif defined(SCATTER)
+    if (rank == root) {
+
+        sbuffer = malloc(factor * count * stride * basic_size * nprocs);
+        rbuffer = malloc(factor * count * blocklength * basic_size);
+
+        for (int i = 0; i < nprocs; i++) {
+            for (int j = 0; j < factor * count * stride; j++) {
+                int pos = i * (factor * count * stride) + j;
+                sbuffer[pos] = (type_basic_t) j + i;
+            }
+        }
+
+        MPI_Scatter(sbuffer, factor, type_derived, rbuffer, factor * count * blocklength,
+                    type_basic, root, MPI_COMM_WORLD);
+
+    } else {
+
+        rbuffer = malloc(factor * count * blocklength * basic_size);
+
+        test_handle(MPI_Scatter
+                    (NULL, 0, MPI_DATATYPE_NULL, rbuffer, factor * count * blocklength, type_basic,
+                     root, MPI_COMM_WORLD));
+
+        for (int i = 0; i < factor * count * blocklength; i++) {
+            test_assert(rbuffer[i] == 1.0 * i + (i / blocklength) * (stride - blocklength) + rank,
+                        "mismatch at position %d", i);
+        }
+    }
+#elif defined(SCATTERV)
+    if (rank == root) {
+
+        sbuffer = malloc(factor * count * stride * basic_size * nprocs);
+        rbuffer = malloc(factor * count * blocklength * basic_size);
+
+        for (int i = 0; i < nprocs; i++) {
+            scounts[i] = factor;
+            sdispls[i] = i * factor;
+        }
+
+        for (int i = 0; i < nprocs; i++) {
+            for (int j = 0; j < factor * count * stride; j++) {
+                int pos = i * (factor * count * stride) + j;
+                sbuffer[pos] = (type_basic_t) j + i;
+            }
+        }
+
+        MPI_Scatterv(sbuffer, scounts, sdispls, type_derived, rbuffer,
+                     factor * count * blocklength, type_basic, root, MPI_COMM_WORLD);
+
+    } else {
+
+        rbuffer = malloc(factor * count * blocklength * basic_size);
+
+        test_handle(MPI_Scatterv
+                    (NULL, NULL, NULL, MPI_DATATYPE_NULL, rbuffer, factor * count * blocklength,
+                     type_basic, root, MPI_COMM_WORLD));
+
+        for (int i = 0; i < factor * count * blocklength; i++) {
+            test_assert(rbuffer[i] == 1.0 * i + (i / blocklength) * (stride - blocklength) + rank,
+                        "mismatch at position %d", i);
+        }
+    }
+#else
+#error "No collop selected!"
+#endif
+
+    /* Clean up global vars */
+    cleanup_datatype();
+
+    MTest_Finalize(errs);
+    return MTestReturnValue(errs);
+}
